2d14012c5b5fa1dab25a01ec2b742deb5c983766
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2026-03-10 17:57:51 -0500

    tuesday update (needs fix)
    why oh why are there <<<<'s and ===='s

--- code.js
+++ code.js
@@ -1,9 +1,15 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 function helloWorld() {
 
   alert("Hello, World!");
+=======
+function toSite(siteLocation) {
+
+  window.location.href = siteLocation;
+>>>>>>> Stashed changes
 =======
 function toSite(siteLocation) {
 
